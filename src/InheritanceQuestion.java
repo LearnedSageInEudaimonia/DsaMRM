f9a9d9757262fa7459bad48dd2854784ba580372
LearnedSage <learnedsageateudaimonia@gmail.com>
2024-12-16 16:34:55 +0530

    Added InheritanceQuestion.java

---
+++ src/InheritanceQuestion.java
@@ -0,0 +1,31 @@
+/* Write a program that consists of three classes, A, B, and C, such that B extends
+A and that C extends B. Each class should define an instance variable named “x”
+(that is, each has its own variable named x). Describe a way for a method in C
+to access and set A’s version of x to a given value, without changing B or C’s
+version.*/
+
+class A{
+    int x=10;
+}
+class B extends A{
+    int x=20;
+}
+class C extends B{
+    int x=30;
+    void setBVersionOfX(int x){
+        super.x = x;
+    }
+    void display(){
+        System.out.println("A version: "+((A)this).x);
+        System.out.println("B version: " + super.x);
+        System.out.println("C version: "+ x);
+    }
+}
+public class InheritanceQuestion {
+    public static void main(String[] args) {
+    C c = new C();
+    c.setBVersionOfX(50);
+    c.display();
+
+    }
+}
